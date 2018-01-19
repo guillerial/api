38e389b02245661a9230599fef3e340bccb51ef4
Guillermo Rial Salgueiro <guillermorial@imasdetres.com>
2018-01-20 00:52:45 +0100

    Middleware SLQAlchemy implemented

--- api/legacy/views.py
+++ api/legacy/views.py
@@ -1,3 +1,41 @@
+from api import settings
 from django.shortcuts import render
+from rest_framework.authtoken.models import Token
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.reverse import reverse
 
 # Create your views here.
+
+class IndexView(APIView):
+
+    def get(self, request):
+
+        urls = {
+            'Hello World': reverse('Hello World', request=request),
+        }
+
+        return Response(urls)
+
+
+index = IndexView.as_view()
+
+
+class HelloWorldView(APIView):
+
+    def get(self, request):
+        session = request.db_session
+        print(session.query())
+
+        return Response()
+
+    def post(self, request):
+
+
+        return Response()
+
+
+hello_world =HelloWorldView.as_view()
