adb5c97bffc4494f6db115a1468b0c5077d225d5
Guillermo Rial Salgueiro <guillermorial@imasdetres.com>
2018-04-03 11:44:32 +0200

    serializer fields

--- api/uvigo/serializers.py
+++ api/uvigo/serializers.py
@@ -62,14 +62,16 @@ class TopologySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Topology
-
+        fields = '__all__'
 
 class IndicationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Indications
+        fields = '__all__'
 
 class ClassroomSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Classroom
+        fields = '__all__'
